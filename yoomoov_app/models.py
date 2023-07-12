64e57123d6dadce661331bc9111da78a7abfb63a
Russ-S <rstanleym74@gmail.com>
2023-07-12 11:48:02 +0000

    build: create initial van model

--- yoomoov_app/models.py
+++ yoomoov_app/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Van(models.Model):
+    """
+    Model for van listings
+    """
+
+    SIZE_CHOICES = (
+        ('SMALL', 'Small'),
+        ('MEDIUM', 'Medium'),
+        ('LARGE', 'Large')
+    )
+
+    LOCATION_CHOICES = (
+        ('LONDON', 'London'),
+        ('MANCHESTER', 'Manchester'),
+        ('BIRMINGHAM', 'Birmingham'),
+        ('LEEDS', 'Leeds'),
+        ('GLASGOW', 'Glasgow'),
+        ('SOUTHAMPTON', 'Southampton'),
+        ('LIVERPOOL', 'Liverpool'),
+        ('NEWCASTLE', 'Newcastle'),
+        ('SHEFFIELD', 'Sheffield'),
+        ('BRIGHTON', 'Brighton'),
+        ('CARDIFF', 'Cardiff'),
+    )
+
+    COUNTY_CHOICES = (
+        ('GREATER LONDON', 'Greater London'),
+        ('GREATER MANCHESTER', 'Greater Manchester'),
+        ('WEST MIDLANDS', 'West Midlands'),
+        ('WEST YORKSHIRE', 'West Yorkshire'),
+        ('GLASGOW CITY', 'Glasgow City'),
+        ('HAMPSHIRE', 'Hampshire'),
+        ('MERSEYSIDE', 'Merseyside'),
+        ('TYNE AND_WARE', 'Tyne & Ware'),
+        ('SOUTH YORKSHIRE', 'South Yorkshire'),
+        ('EAST SUSSEX', 'East Sussex'),
+        ('CARDIFF', 'Cardiff'),
+    )
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    size = models.CharField(max_length=75, choices=SIZE_CHOICES)
+    location = models.CharField(max_length=75, choices=LOCATION_CHOICES)
+    county = models.CharField(max_length=75, choices=COUNTY_CHOICES)
+    crew = models.IntegerField(default=1)
+    suitable_for = models.TextField(max_length=200)
+    load_area_width = models.IntegerField(verbose_name='Load Area Width in metres')
+    load_area_height = models.IntegerField(verbose_name='Load Area Height in metres')
+    load_area_length = models.IntegerField(verbose_name='Load Area Length in metres')
+    image = CloudinaryField('image', default='placeholder')
+    date_added = models.DateTimeField(default=datetime.now, blank=True)
+    is_live = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        Meta class for ordering data set by latest date-added
+        """
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return self.name
+
