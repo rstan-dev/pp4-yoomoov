fbd0bb2d5470128e37a915d02324a4604a493650
Russ-S <rstanleym74@gmail.com>
2023-07-12 12:03:28 +0000

    build: register Van model in admin.py

--- yoomoov_app/admin.py
+++ yoomoov_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Van
 
-# Register your models here.
+@admin.register(Van)
+class VanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'size', 'location', 'crew', 'date_added', 'is_live' )
