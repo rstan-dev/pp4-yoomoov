a8d25d7125bc9246dfead26ea90445caac5a58a7
Russ-S <rstanleym74@gmail.com>
2023-07-13 10:55:55 +0000

    feat: create view to display van list

--- yoomoov_app/views.py
+++ yoomoov_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Van
+
 
 def home(request):
     return render(request, 'index.html')
+
+class VanList(generic.ListView):
+    model = Van
+    queryset = Van.objects.filter(is_live=True).order_by('-date_added')
+    template_name = 'index.html'
+    paginate_by = 3
