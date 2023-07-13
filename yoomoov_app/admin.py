117c3b034016a7adc99066f9996946740fca33a9
Russ-S <rstanleym74@gmail.com>
2023-07-13 16:22:17 +0000

    feat: add filters and links to admin dashboard. Completes UserStory #8

--- yoomoov_app/admin.py
+++ yoomoov_app/admin.py
@@ -5,3 +5,7 @@ from .models import Van
 @admin.register(Van)
 class VanAdmin(admin.ModelAdmin):
     list_display = ('name', 'size', 'location', 'crew', 'date_added', 'is_live')
+    list_display_links = ('name',)
+    list_filter = ('size', 'location', 'crew', 'date_added')
+    list_editable = ('is_live',)
+    search_fields = ('name', 'size', 'location', 'county')
