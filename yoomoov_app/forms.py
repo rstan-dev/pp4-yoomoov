574272c9d35b68fd3ceb2338c8e12f18ddde56af
Russ-S <rstanleym74@gmail.com>
2023-07-24 09:34:08 +0000

    feat: US13 - prevent booking past dates, update forms.py - add date input widget and clean_date_required function

--- yoomoov_app/forms.py
+++ yoomoov_app/forms.py
@@ -1,8 +1,32 @@
+from django import forms
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 from .models import Booking
 
 
-class BookingForm(ModelForm):
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+    def __init__(self, **kwargs):
+        kwargs['attrs'] = {'min': timezone.localdate().isoformat()}
+        super().__init__(**kwargs)
+
+
+class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['first_name', 'last_name', 'email', 'phone', 'van', 'date_required']
+        widgets = {
+            'date_required': DateInput(),
+        }
+
+    def clean_date_required(self):
+        date_required = self.cleaned_data.get('date_required')
+        if date_required and date_required < timezone.localdate():
+            raise ValidationError("Please select a future date")
+        return date_required
+
+
+
+
