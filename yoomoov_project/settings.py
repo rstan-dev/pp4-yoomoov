ac03e7a2e56ed7feb257c40bb738efcc8e8af7c2
Russ-S <rstanleym74@gmail.com>
2023-07-27 08:29:36 +0000

    feat: US19 - setup email config, add pw to env.py

--- yoomoov_project/settings.py
+++ yoomoov_project/settings.py
@@ -200,3 +200,11 @@ LOGGING = {
         },
     },
 }
+
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST_USER = 'yoomoov@outlook.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
