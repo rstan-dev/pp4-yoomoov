03770ed5b687b048c33ea38868defb115b6d4cf1
Russ-S <rstanleym74@gmail.com>
2023-08-07 11:39:07 +0000

    fix: US26 - update urls and models to pass python linter

--- yoomoov_app/models.py
+++ yoomoov_app/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 from cloudinary.models import CloudinaryField
-from .choices import SIZE_CHOICES, LOCATION_CHOICES, COUNTY_CHOICES, STATUS_CHOICES, RATING_CHOICES, APPROVAL_CHOICES
 from django.core.mail import send_mail
+from .choices import (
+    SIZE_CHOICES,
+    LOCATION_CHOICES,
+    COUNTY_CHOICES,
+    STATUS_CHOICES,
+    RATING_CHOICES,
+    APPROVAL_CHOICES
+)
 
 
 class Van(models.Model):
--- yoomoov_app/urls.py
+++ yoomoov_app/urls.py
@@ -12,8 +12,10 @@ urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('create_booking/', views.createBooking, name='create_booking'),
     path('edit_booking/<str:pk>', views.editBooking, name='edit_booking'),
-    path('delete_booking/<str:pk>', views.deleteBooking, name='delete_booking'),
-    path('leave_feedback/<str:pk>', views.leaveFeedback, name='leave_feedback'),
+    path('delete_booking/<str:pk>', views.deleteBooking,
+         name='delete_booking'),
+    path('leave_feedback/<str:pk>', views.leaveFeedback,
+         name='leave_feedback'),
     path('<slug:slug>/', views.van_detail, name='van_detail'),
     path('accounts/login/', CustomLoginView.as_view(), name='account_login'),
 ]
