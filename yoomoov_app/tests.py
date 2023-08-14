254be326b484b7e56b1c6d6f03165ce78d750dcd
Russ-S <rstanleym74@gmail.com>
2023-08-14 16:14:25 +0000

    test: create test for successful Get request, move user login setup to CreateBooking test class.  Move create_booking redirect to Errorhandlers class for readability

--- yoomoov_app/tests.py
+++ yoomoov_app/tests.py
@@ -7,7 +7,24 @@ from django.http import HttpResponseServerError
 
 
 class ErrorHandlersTest(TestCase):
-    # Creates a user and logs the test user in to test the 404 redirect
+
+    # Test that a 404 error message redirects the user to the login page
+    def test_handler404(self):
+        response = self.client.get('/edit_booking/63')
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse('account_login'), response['Location'])
+
+    # Tests for a redirection if a user is not logged in
+    def test_create_booking_when_not_logged_in(self):
+        response = self.client.get(reverse('create_booking'))
+        self.assertRedirects(response,
+                             '/accounts/login/?next=%2Fcreate_booking%2F')
+
+
+class CreateBookingTests(TestCase):
+
+    # Creates a user and logs the test user in to test the CreateBooking
+    # functions
     def setUp(self):
         self.user = User.objects.create_user(
             username='testuser',
@@ -16,17 +33,10 @@ class ErrorHandlersTest(TestCase):
         self.client = Client()
         self.client.login(username='testuser', password='testpassword')
 
-    # Test that a 404 error message redirects the user to the login page
-    def test_handler404(self):
-        response = self.client.get('/edit_booking/63')
-        self.assertEqual(response.status_code, 302)
-        self.assertIn(reverse('account_login'), response['Location'])
-
-
-class CreateBookingTests(TestCase):
-
-    # Tests for a redirection if a user is not logged in
-    def test_create_booking_when_not_logged_in(self):
+    # Tests for a succesfuly GET requet when user is logged in
+    def test_create_booking_get_request_user_logged_in(self):
+        self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('create_booking'))
-        self.assertRedirects(response,
-                             '/accounts/login/?next=%2Fcreate_booking%2F')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dashboard.html')
+
