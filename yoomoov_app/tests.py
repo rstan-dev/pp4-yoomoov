10b5fecd72f6b08fbe01b6a03b3614992546e3c3
Russ-S <rstanleym74@gmail.com>
2023-08-16 16:28:31 +0000

    test: create deleteBooking test, assert the GET and POST request functions as expected and returns correct status codes

--- yoomoov_app/tests.py
+++ yoomoov_app/tests.py
@@ -120,11 +120,62 @@ class EditBookingTests(TestCase):
         }
 
         response = self.client.post(reverse('edit_booking', args=[self.booking.id]), updated_data)
-
+        # Checks for a successful redirect code
         self.assertEqual(response.status_code, 302)
-
+        # Checks if the data up[dated successfully]
         updated_booking = Booking.objects.get(id=self.booking.id)
         self.assertEqual(updated_booking.first_name, 'Updated_Name')
+
+
+class DeleteBookingTests(TestCase):
+
+    # Creates a user and logs the test user in to test the deleteBooking
+    # functions
+    # Creates a Van Object and a Booking object to test functions
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        self.van = Van.objects.create(
+            name='Large Van in Sheffield',
+            slug='large_van_sheffield',
+            size='Large',
+            location='Sheffield',
+            county='South Yorkshire',
+            crew=1,
+            suitable_for='Suitable description',
+            load_area_width=2.5,
+            load_area_height=2.5,
+            load_area_length=2.5,
+            price='250.00',
+            is_live=True
+        )
+
+        self.booking = Booking.objects.create(
+            booking_number='1500',
+            van=self.van,
+            first_name='TestCustomer',
+            last_name='TestSurname',
+            email='customer@email.com',
+            phone='123456789',
+            date_required='2030-01-01',
+        )
+
+        self.client = Client()
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_delete_booking_successful_get_request(self):
+        # Tests the GET request and checks booking form is prefilled corectly
+        response = self.client.get(reverse('delete_booking', args=[self.booking.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_booking_successful(self):
+        # Tests the POST request to the delete_booking view is successful
+        response = self.client.post(reverse('delete_booking', args=[self.booking.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('dashboard'))
 
 
 
