001ac45cda77656a7c8b204d50ce880cf00fbcad
Russ-S <rstanleym74@gmail.com>
2023-08-16 15:45:49 +0000

    test: create EditBookingTests case, test for successful GET request

--- yoomoov_app/tests.py
+++ yoomoov_app/tests.py
@@ -4,6 +4,9 @@ from django.contrib.messages import get_messages
 from django.contrib.auth.models import User
 from django.test import Client
 from django.http import HttpResponseServerError
+from django.core.mail import send_mail
+from unittest.mock import patch
+from .models import Van, Booking
 
 
 class ErrorHandlersTest(TestCase):
@@ -40,7 +43,7 @@ class CreateBookingTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'dashboard.html')
 
-    # Tests for sucessful POST request when user is logged in
+    # Tests for sucessful POST request when user is logged in and
     def test_create_booking_post_request_user_logged_in(self):
         self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('create_booking'))
@@ -60,4 +63,48 @@ class CreateBookingTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'dashboard.html')
 
+
+class EditBookingTests(TestCase):
+
+    # Creates a user and logs the test user in to test the editBooking
+    # functions
+    # Creates a Van Object and a Booking object to test functions
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        self.van = Van.objects.create(
+            name='Large Van in Sheffield',
+            slug='large_van_sheffield',
+            size='Large',
+            location='Sheffield',
+            county='South Yorkshire',
+            crew=1,
+            suitable_for='Suitable description',
+            load_area_width=2.5,
+            load_area_height=2.5,
+            load_area_length=2.5,
+            price='250.00',
+            is_live=True
+        )
+
+        self.booking = Booking.objects.create(
+            booking_number='1500',
+            van=self.van,
+            first_name='TestCustomer',
+            last_name='TestSurname',
+            email='customer@email.com',
+            phone='123456789',
+            date_required='2030-01-01',
+        )
+
+        self.client = Client()
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_edit_booking(self):
+        # Tests the GET request and checks booking form is prefilled corectly
+        response = self.client.get(reverse('edit_booking', args=[self.booking.id]))
+        self.assertEqual(response.status_code, 200)
 
