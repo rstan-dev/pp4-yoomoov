f5eb0a622b96eecf5482895a674f5c2410f454bd
Russ-S <rstanleym74@gmail.com>
2023-08-17 14:18:36 +0000

    test: update  ContactFormTest, add assertion to check if success message is displayed corectly, test if redirection back to van_detail with slug is successful

--- yoomoov_app/tests.py
+++ yoomoov_app/tests.py
@@ -371,19 +371,59 @@ class ContactFormTest(TestCase):
     # for if there is a slug, and if so the redirect should be back to the
     # van page or redirect to the home page.
 
+    def setUp(self):
+        # Creates a Van Object to test contact form with slug
+        date_added_3 = datetime(2024, 1, 2, 12, 0)
+
+        self.vans = [
+            Van.objects.create(
+                name='test Van 1',
+                slug='test_van_1',
+                size='Small',
+                location='London',
+                county='Greater London',
+                crew=1,
+                suitable_for='Suitable description',
+                load_area_width=2.5,
+                load_area_height=2.5,
+                load_area_length=2.5,
+                price='250.00',
+                is_live=True,
+                date_added=date_added_3
+            )
+        ]
+
+        self.form_data = {
+            'name': 'Test name',
+            'email': 'test@email.com',
+            'message': 'test message left'
+        }
+
     # Test to ensure the correct template is used
     def test_contact_form_template_successful(self):
         response = self.client.get(reverse('contact'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact.html')
 
-    # Test to ensure the POST request is successful
+    # Test to ensure the POST request is successful and a sucess
+    # message is displayed after redirect
     def test_contact_form_post_successful(self):
-        form_data = {
-            'name': 'Test name',
-            'email': 'test@email.com',
-            'message': 'test message left'
-        }
+        response = self.client.post(reverse('contact'),
+                                    self.form_data, follow=True)
 
-        response = self.client.post(reverse('contact'), form_data)
-        self.assertEqual(response.status_code, 302)
+        messages = list(response.context['messages'])
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "Your message has been sent! "
+                                           "We will respond within 24 hours.")
+
+    # Test to check contact form redirects back to correct van_detail page
+    # using the slug
+    def test_contact_form_slug_redirects_back_to_van_detail_page(self):
+        response = self.client.post(reverse('contact_from_van',
+                                            kwargs={'slug': 'test_van_1'}),
+                                    self.form_data)
+        self.assertRedirects(response, reverse('van_detail',
+                                               kwargs={'slug': 'test_van_1'}))
+
+
+
